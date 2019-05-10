83eac2fc7158e600a2820eab09c04b323b657463
Yashashvi Dave <yashashvidave@gmail.com>
2019-05-10 19:07:00 +0530

    Add comments to stellar motion script.

--- stellar_motion/stellar_motion.m
+++ stellar_motion/stellar_motion.m
@@ -1,18 +1,39 @@
+% Course: https://matlabacademy.mathworks.com/R2018b/portal.html?course=gettingstarted
+
+% To calculate the velocity how much the star is away from earth
+% A faint start HD 94028 in the constellation of Leo
+% Variables loaded from 'electricy.mat'
+% Name     : Size     : Description
+% spectra  : 357 * 10 : evenly-spaced wavelengths data of different stars
+
+% Clear variables
+clc;
+clear;
+
+% Load data from .mat file
 load('spectrum_data.mat', '-mat');
-nObs = size(spectra,1);
-lambdaStart = 630.02;
-lambdaDelta = 0.14;
-lambdaEnd = lambdaStart + (nObs - 1) * lambdaDelta;
 
-lambda = [lambdaStart:lambdaDelta:lambdaEnd]';
+% Given observation data
+% No of observation
+no_observations = size(spectra, 1);
+% Starting wavelength
+lambdas_start = 630.02;
+% Spacing between wavelength
+lambdad_delta = 0.14;
 
-s = spectra(: ,6);
+% Calculate other required data from given data
+lambda_end = lambdas_start + (no_observations - 1) * lambdad_delta;
+% lambda (λ) containing the wavelengths in the spectrum, from λstart to λend, in steps of λdelta
+lambda = [lambdas_start : lambdad_delta : lambda_end]';
+% sixth column is the wavelength spectrum of star HD 94028
+spectrum_data = spectra(: ,6);
 
-plot(lambda, s, 'b.-');
+% Plot wavelength spectrum to visualize the spike in spectrum
+plot(lambda, spectrum_data, 'b.-');
 grid on;
 xlabel('Wavelength')
 ylabel('Intensity')
 
-[sHa, idx] = min(s);
-lambdaHa = lambda(idx);
+[spectrum_Ha, idx] = min(spectrum_data);
+lambda_for_Ha = lambda(idx);
 
